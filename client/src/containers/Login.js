46fc1ebb03be1ae7955c5628eb3a98b218473aef
shekhar771 <ashekhar.1239@gmail.com>
2022-11-02 20:59:52 +0530

    some

--- client/src/containers/Login.js
+++ client/src/containers/Login.js
@@ -1,8 +1,9 @@
-import { Button, Col, Form, Input, message, Row } from "antd";
-import React from "react";
-import { useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
-import useCustomers from "../_actions/customerActions";
+import { Button, Col, Form, Input, message, Row } from 'antd';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import useCustomers from '../_actions/customerActions';
+import { Navigate } from 'react-router-dom';
 
 function Login() {
   const [form] = Form.useForm();
@@ -10,10 +11,10 @@ function Login() {
   const { customerLogin } = useCustomers();
   const onFinish = (value) => {
     dispatch(customerLogin(value)).then((res) => {
-      console.log("REs: ", res);
+      console.log('REs: ', res);
       if (res.payload.status) {
         const token = res.payload.data.token;
-        localStorage.setItem("customerToken", token);
+        localStorage.setItem('customerToken', token);
         message.success(res.payload.message);
       } else {
         message.error(res.payload.message);
@@ -21,29 +22,29 @@ function Login() {
     });
   };
   return (
-    <div className="page-wrapper">
-      <Row justify="center">
+    <div className='page-wrapper'>
+      <Row justify='center'>
         <Col xs={24} sm={16} md={12} lg={8} xl={6} xxl={6}>
           <h2>Login</h2>
           <Form
             form={form}
-            name="register"
-            layout="vertical"
-            autoComplete="off"
+            name='register'
+            layout='vertical'
+            autoComplete='off'
             onFinish={onFinish}
             scrollToFirstError
           >
             <Form.Item
-              name="email"
-              label="E-mail"
+              name='email'
+              label='E-mail'
               rules={[
                 {
-                  type: "email",
-                  message: "The input is not valid E-mail!",
+                  type: 'email',
+                  message: 'The input is not valid E-mail!',
                 },
                 {
                   required: true,
-                  message: "Please input your E-mail!",
+                  message: 'Please input your E-mail!',
                 },
               ]}
             >
@@ -51,12 +52,12 @@ function Login() {
             </Form.Item>
 
             <Form.Item
-              name="password"
-              label="Password"
+              name='password'
+              label='Password'
               rules={[
                 {
                   required: true,
-                  message: "Please input your password!",
+                  message: 'Please input your password!',
                 },
               ]}
               hasFeedback
@@ -65,14 +66,14 @@ function Login() {
             </Form.Item>
 
             <Form.Item>
-              <Link to="/forgotPassword">Forgot Password</Link>
-              <Link to="/register" style={{ float: "right" }}>
+              <Link to='/forgotPassword'>Forgot Password</Link>
+              <Link to='/register' style={{ float: 'right' }}>
                 Create new account
               </Link>
             </Form.Item>
 
             <Form.Item>
-              <Button type="primary" htmlType="submit">
+              <Button type='primary' htmlType='submit'>
                 Login
               </Button>
             </Form.Item>
